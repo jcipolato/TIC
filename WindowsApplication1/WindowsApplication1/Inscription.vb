5c628df393a85b29b85aa26b6ba3e9e1abd8c819
Clara <Clara@PORTABLECLARA>
2019-03-18 11:53:45 +0100

    Récupération données formulaire

--- WindowsApplication1/WindowsApplication1/Inscription.vb
+++ WindowsApplication1/WindowsApplication1/Inscription.vb
@@ -48,4 +48,67 @@
                                                 End Sub
         Next
     End Sub
+
+    Private Sub btnSauvegarder_Click(sender As Object, e As EventArgs) Handles btnSauvegarder.Click
+        Dim dossier As New Dossier()
+        dossier.p_civilite = cbCivilite.SelectedValue
+        dossier.p_nom = txtNom.Text
+        dossier.p_prenom = txtPrenom.Text
+        dossier.p_dateNaissance = txtDateNaissance.Text
+        dossier.p_adresse = txtAdresse.Text
+        dossier.p_complementAdresse = txtComplement.Text
+        dossier.p_codePostal = txtCP.Text
+        dossier.p_ville = txtVille.Text
+        dossier.p_pays = cbPays.SelectedValue
+        dossier.p_Statut = cbStatut.SelectedValue
+        dossier.p_niveauEtudes = cbEtudes.SelectedValue
+        dossier.p_autre = txtAutre.Text
+        dossier.p_profession = txtProfession.Text
+        dossier.p_domaineEmploi = cbEmploi.SelectedValue
+        dossier.p_mailPerso = txtMailPerso.Text
+        dossier.p_mailPro = txtMailPro.Text
+        dossier.p_telPerso = txtTelPerso.Text 'pas de conversion à faire ?
+        dossier.p_telPro = txtTelPro.Text
+
+        If rbOuiCV.Checked Then
+            dossier.p_cvOuiNon = True
+        Else dossier.p_cvOuiNon = False
+        End If
+
+        'dossier.p_cvOuiNon = rbOuiCV.Checked Or rbNonCV.Checked à tester
+
+        If rbOuiMotiv.Checked Then
+            dossier.p_lettreMotivationOuiNon = True
+        Else dossier.p_lettreMotivationOuiNon = False
+        End If
+
+        dossier.p_dateDebutContrat = dtDebutContrat.Value
+        dossier.p_dateFinContrat = dtFinContrat.Value
+
+        If rbAcompteOui.Checked Then
+            dossier.p_accompteOuiNon = True
+        Else dossier.p_accompteOuiNon = False
+        End If
+
+        dossier.p_numeroCheque = txtCheque.Text
+        dossier.p_financement = cbFinancement.SelectedValue
+        dossier.p_fidelite = cbFidelite.SelectedValue
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub rbOuiCV_CheckedChanged(sender As Object, e As EventArgs) Handles rbOuiCV.CheckedChanged
+        ' MessageBox pour afficher p_CVOuiNon
+
+
+    End Sub
 End Class

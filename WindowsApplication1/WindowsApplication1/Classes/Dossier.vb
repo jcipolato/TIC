e85c21979eb6f5e4da07119dfba2ceaccc850457
Clara <Clara@PORTABLECLARA>
2019-03-18 09:34:49 +0100

    Classe Dossier

--- WindowsApplication1/WindowsApplication1/Classes/Dossier.vb
+++ WindowsApplication1/WindowsApplication1/Classes/Dossier.vb
@@ -1,140 +1,268 @@
 ﻿Public Class Dossier
 
-    Private civilite As String
-    Private nom As String
-    Private prenom As String
-    Private dateNaissance As DateTime
-    Private adresse As String
-    Private codePostal As Integer
-    Private ville As String
-    Private pays As String
-    Private statut As String
-    Private niveauEtudes As String
-    Private autre As String
-    Private profession As String
-    Private domaineEmploi As String
-    Private mailPerso As String
-    Private mailPro As String
-    Private telPerso As Integer
-    Private telPro As Integer
-    Private cvOuiNon As Boolean
-    Private lettreMotivationOuiNon As Boolean
-    Private dateDebutContrat As DateTime
-    Private dateFinContrat As DateTime
-    Private accompteVerseOuiNon As Boolean
-    Private numeroCheque As Integer
-    Private typeFinancement As String
-    Private fidelite As String
 
     Sub New()
         'Constructeur
     End Sub
 
-    'Famille des get
+    Private civilite As String
+    Public Property p_civilite() As String
+        Get
+            Return civilite
+        End Get
+        Set(ByVal value As String)
+            civilite = value
+        End Set
+    End Property
 
-    Private Function getCivilite()
-        Return civilite
-    End Function
+    Private nom As String
+    Public Property p_nom() As String
+        Get
+            Return nom
+        End Get
+        Set(ByVal value As String)
+            nom = value
+        End Set
+    End Property
 
-    Private Function getNom()
-        Return nom
-    End Function
+    Private prenom As String
+    Public Property p_prenom() As String
+        Get
+            Return prenom
+        End Get
+        Set(ByVal value As String)
+            prenom = value
+        End Set
+    End Property
 
-    Private Function getPrenom()
-        Return prenom
-    End Function
+    Private dateNaissance As DateTime
+    Public Property p_dateNaissance() As String
+        Get
+            Return dateNaissance
+        End Get
+        Set(ByVal value As String)
+            dateNaissance = value
+        End Set
+    End Property
 
-    Private Function getDateNaissance()
-        Return dateNaissance
-    End Function
+    Private adresse As String
+    Public Property p_adresse() As String
+        Get
+            Return adresse
+        End Get
+        Set(ByVal value As String)
+            adresse = value
+        End Set
+    End Property
 
-    Private Function getAdresse()
-        Return adresse
-    End Function
+    Private complementAdresse As String
+    Public Property p_complementAdresse() As String
+        Get
+            Return complementAdresse
+        End Get
+        Set(ByVal value As String)
+            complementAdresse = value
+        End Set
+    End Property
 
-    Private Function getCodePostal()
-        Return codePostal
-    End Function
+    Private codePostal As Integer
+    Public Property p_codePostal() As Integer
+        Get
+            Return codePostal
+        End Get
+        Set(ByVal value As Integer)
+            codePostal = value
+        End Set
+    End Property
 
-    Private Function getVille()
-        Return ville
-    End Function
+    Private ville As String
+    Public Property p_ville() As String
+        Get
+            Return ville
+        End Get
+        Set(ByVal value As String)
+            ville = value
+        End Set
+    End Property
 
-    Private Function getPays()
-        Return pays
-    End Function
+    Private pays As String
+    Public Property p_pays() As String
+        Get
+            Return pays
+        End Get
+        Set(ByVal value As String)
+            pays = value
+        End Set
+    End Property
 
-    Private Function getStatut()
-        Return statut
-    End Function
+    Private Statut As String
+    Public Property p_Statut() As String
+        Get
+            Return Statut
+        End Get
+        Set(ByVal value As String)
+            Statut = value
+        End Set
+    End Property
 
-    Private Function getNiveauEtude()
-        Return niveauEtudes
-    End Function
+    Private niveauEtudes As String
+    Public Property p_niveauEtudes() As String
+        Get
+            Return niveauEtudes
+        End Get
+        Set(ByVal value As String)
+            niveauEtudes = value
+        End Set
+    End Property
 
-    Private Function getAutre()
-        Return autre
-    End Function
+    Private autre As String
+    Public Property p_autre() As String
+        Get
+            Return autre
+        End Get
+        Set(ByVal value As String)
+            autre = value
+        End Set
+    End Property
 
-    Private Function getProfession()
-        Return profession
-    End Function
+    Private profession As String
+    Public Property p_profession() As String
+        Get
+            Return profession
+        End Get
+        Set(ByVal value As String)
+            profession = value
+        End Set
+    End Property
 
-    Private Function getDomaineEmploi()
-        Return domaineEmploi
-    End Function
+    Private domaineEmploi As String
+    Public Property p_domaineEmploi() As String
+        Get
+            Return domaineEmploi
+        End Get
+        Set(ByVal value As String)
+            domaineEmploi = value
+        End Set
+    End Property
 
-    Private Function getMailPerso()
-        Return mailPerso
-    End Function
+    Private mailPerso As String
+    Public Property p_mailPerso() As String
+        Get
+            Return mailPerso
+        End Get
+        Set(ByVal value As String)
+            mailPerso = value
+        End Set
+    End Property
 
-    Private Function getMailPro()
-        Return mailPro
-    End Function
+    Private mailPro As String
+    Public Property p_mailPro() As String
+        Get
+            Return mailPro
+        End Get
+        Set(ByVal value As String)
+            mailPro = value
+        End Set
+    End Property
 
-    Private Function getTelPerso()
-        Return telPerso
-    End Function
+    Private telPro As Integer
+    Public Property p_telPro() As Integer
+        Get
+            Return telPro
+        End Get
+        Set(ByVal value As Integer)
+            telPro = value
+        End Set
+    End Property
 
-    Private Function getTelPro()
-        Return telPro
-    End Function
+    Private telPerso As Integer
+    Public Property p_telPerso() As Integer
+        Get
+            Return telPerso
+        End Get
+        Set(ByVal value As Integer)
+            telPerso = value
+        End Set
+    End Property
 
-    Private Function getCV()
-        Return cvOuiNon
-    End Function
+    Private cvOuiNon As Boolean
+    Public Property p_cvOuiNon() As Boolean
+        Get
+            Return cvOuiNon
+        End Get
+        Set(ByVal value As Boolean)
+            cvOuiNon = value
+        End Set
+    End Property
 
-    Private Function getLettreMotivation()
-        Return lettreMotivationOuiNon
-    End Function
+    Private lettreMotivationOuiNon As Boolean
+    Public Property p_lettreMotivationOuiNon() As Boolean
+        Get
+            Return lettreMotivationOuiNon
+        End Get
+        Set(ByVal value As Boolean)
+            lettreMotivationOuiNon = value
+        End Set
+    End Property
 
-    Private Function getDateDebutContrat()
-        Return dateDebutContrat
-    End Function
+    Private dateDebutContrat As DateTime
+    Public Property p_dateDebutContrat() As DateTime
+        Get
+            Return dateDebutContrat
+        End Get
+        Set(ByVal value As DateTime)
+            dateDebutContrat = value
+        End Set
+    End Property
 
-    Private Function getDateFinContrat()
-        Return dateFinContrat
-    End Function
+    Private dateFinContrat As DateTime
+    Public Property p_dateFinContrat() As DateTime
+        Get
+            Return dateFinContrat
+        End Get
+        Set(ByVal value As DateTime)
+            dateFinContrat = value
+        End Set
+    End Property
 
-    Private Function getAccompte()
-        Return accompteVerseOuiNon
-    End Function
+    Private accompteOuiNon As Boolean
+    Public Property p_accompteOuiNon() As Boolean
+        Get
+            Return accompteOuiNon
+        End Get
+        Set(ByVal value As Boolean)
+            accompteOuiNon = value
+        End Set
+    End Property
 
-    Private Function getNumeroCheque()
-        Return numeroCheque
-    End Function
+    Private numeroCheque As Integer
+    Public Property p_numeroCheque() As Integer
+        Get
+            Return numeroCheque
+        End Get
+        Set(ByVal value As Integer)
+            numeroCheque = value
+        End Set
+    End Property
 
-    Private Function getTypeFinancement()
-        Return typeFinancement
-    End Function
+    Private financement As String
+    Public Property p_financement() As String
+        Get
+            Return financement
+        End Get
+        Set(ByVal value As String)
+            financement = value
+        End Set
+    End Property
 
-    Private Function getFidelite()
-        Return fidelite
-    End Function
-
-    'Famille des get
-
-    'Private Function setCivilite(chercher syntaxe lundi)
-
+    Private fidelite As String
+    Public Property p_fidelite() As String
+        Get
+            Return fidelite
+        End Get
+        Set(ByVal value As String)
+            fidelite = value
+        End Set
+    End Property
 
 End Class
